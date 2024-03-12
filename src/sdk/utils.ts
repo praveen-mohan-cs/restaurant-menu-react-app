e12af74dadd78dfddba1e22c64537245b83cfd66
Praveen Mohan <praveen.mohan@contentstack.com>
2024-03-12 14:37:12 +0530

    Code cleanup

--- src/sdk/utils.ts
+++ src/sdk/utils.ts
@@ -2,14 +2,14 @@ import Contentstack from "contentstack";
 
 export const initializeContentstackSdk = () => {
   const {
-    REACT_APP_ENV_CONTENTSTACK_API_KEY,
-    REACT_APP_ENV_CONTENTSTACK_DELIVERY_TOKEN,
-    REACT_APP_ENV_CONTENTSTACK_ENVIRONMENT,
+    CONTENTSTACK_API_KEY,
+    CONTENTSTACK_DELIVERY_TOKEN,
+    CONTENTSTACK_ENVIRONMENT,
   } = process.env;
   const Stack = Contentstack.Stack({
-    api_key: REACT_APP_ENV_CONTENTSTACK_API_KEY as string,
-    delivery_token: REACT_APP_ENV_CONTENTSTACK_DELIVERY_TOKEN as string,
-    environment: REACT_APP_ENV_CONTENTSTACK_ENVIRONMENT as string,
+    api_key: CONTENTSTACK_API_KEY as string,
+    delivery_token: CONTENTSTACK_DELIVERY_TOKEN as string,
+    environment: CONTENTSTACK_ENVIRONMENT as string,
   });
   return Stack;
 };
